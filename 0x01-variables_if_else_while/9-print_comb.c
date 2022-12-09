63531b139a5dcf8f7c071b50a23ad58ea4c3414d
Babajide <babajideoloro@gmail.com>
2022-12-09 05:30:23 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+
+}
