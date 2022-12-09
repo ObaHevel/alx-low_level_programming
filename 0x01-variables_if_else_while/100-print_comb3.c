d70e954affc36c3fbbaf8bd3183fb45ebc28a21c
Babajide <babajideoloro@gmail.com>
2022-12-09 05:43:07 -0800

    Inventing

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible diferent combinations of two digits
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+	int ones = '0';
+	int tens = '0';
+
+	for (tens = '0'; tens <= '9'; tens++)/* prints tens*/
+	{
+		for (ones ='0'; ones <= '9'; ones++)/* prints ones*/
+		{
+			if (!((ones == tens) || tens > ones))/*stops repitition*/
+			{
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && tens == '8'))/*adds coma and space*/
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
