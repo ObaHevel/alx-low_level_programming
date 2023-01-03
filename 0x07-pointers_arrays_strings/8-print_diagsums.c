995de654e4ee599169d860394b632c4cecf333cf
Babajide <babajideoloro@gmail.com>
2023-01-03 07:27:40 -0800

    More strings

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,22 +9,18 @@
 
 void print_diagsums(int *a, int size)
 {
-	int diagonal_sum_1 = 0;
-	int diagonal_sum_2 = 0;
-	int row, i;
+	int i, S1, S2;
 
-	for (row = 0; row < size; row++)
+	S1 = 0;
+	S2 = 0;
+
+	for (i = 0; i < (size * size); i++)
 	{
-		i = (row * size) + row;
-		diagonal_sum_1 += a[i];
+		if (i % (size + 1) == 0)
+			S1 += a[i];
+		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+			S2 += a[i];
 	}
-
-	for (row = 1; row <= size; row++)
-	{
-		i = (row * size) - row;
-		diagonal_sum_2 += a[i];
-	}
-
-	printf("%d, %d\n", diagonal_sum_1, diagonal_sum_2)
-
+	printf("%d, %d\n", S1, S2);
 }
+
